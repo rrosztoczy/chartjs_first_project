252a86cd7aaa37e3c713ac44ea405d4c0f360d4e
rrosztoczy <rrosztoczy@gmail.com>
2019-05-01 20:27:50 -0400

    update

--- index.js
+++ index.js
@@ -5,68 +5,87 @@ const ctx = canvas.getContext('2d');
 
 (async () => {
     const parsedData = await d3.csv("crime.csv")
-    const offenseType = await parsedData.map((crime) => crime.OFFENSE_TYPE_ID)
-    const neighborhood = await parsedData.map((crime) => crime.NEIGHBORHOOD_ID)
-    const isCrime = await parsedData.map((crime) => crime.CRIME_COUNT)
-    const neighborhoodIds = await parsedData.map(crime => crime.NEIGHBORHOOD_ID)
-    const crimeTypes = await parsedData.map(crime => crime.OFFENSE_TYPE_ID)
+    const offenseType = await parsedData.map((crime) => crime.y)
+    const neighborhood = await parsedData.map((crime) => crime.x)
+    const isCrime = await parsedData.map((crime) => crime.r)
+
+    // arrays for axis labels and numeric mapping
+    const neighborhoodIds = await parsedData.map(crime => crime.x)
+    const crimeTypes = await parsedData.map(crime => crime.y)
     const xAxisNeighborhood = await [...new Set(neighborhoodIds)]
     const yAxisCrimeType = await [...new Set(crimeTypes)]
+
+    // Map categories to numbers for ChartJs
+    const xAxis = {}
+    const yAxis = {}
+    const mappedXAxisNeighborhood = await xAxisNeighborhood.forEach((neighborhood, i) => (xAxis[neighborhood] = i))
+    const mappedYAxisCrimeType = await yAxisCrimeType.forEach((crimeType, i) => (yAxis[crimeType] = i)) 
+    console.log("mapped x axis", mappedXAxisNeighborhood)
+    console.log("mapped y axis", mappedYAxisCrimeType)
+
+    function replaceCategoryWithKey(string, mappingArray) {
+        // Search through mappingArray for the key that matches string, return the value at that key
+        mappingArray.forEach(element => {
+            if (element[string]) {return element[string]}
+        })
+    }
+
+    // Categories are mapped, now I want to take formatted data and replace it with mapped data that replaces the value of each key based
+    // on the map
+    const formattedData = parsedData.map(dataObj => ({x: dataObj.x, y: dataObj.y, r: parseInt(dataObj.r)}))
+    const mappedData = formattedData.map(dataObj => ({x: replaceCategoryWithKey(dataObj.x, mappedXAxisNeighborhood), y: replaceCategoryWithKey(dataObj.y, mappedYAxisCrimeType), r: dataObj.r}))
+    console.table("formatted", formattedData)
+    console.table("mapped", mappedData)
+    // For this map, go through formatted data. For each string that will be x/y, replace the key with x or y, and turn the value into the mapped number
+    // What is the mapped number? Each "mapped" aray is a string key and then a number. want to find the string key
+    // In formatted data, and replace it with a number
+    
+    // // console.log(dataObj.x)
+    // // console.log(dataObj.y)
+    // console.dir(mappedData)
+
+
     console.dir(yAxisCrimeType)
     console.dir(xAxisNeighborhood)
     //TODO: need to replace x, y and r labels with x y and r
-    const formattedData = await parsedData.map(dataObj => ({"label": "none", "data": [{...dataObj}], "backgroundColor": "#ff6384", 
-    "hoverBackgroundColor": "#ff6384"}))
-    const myChart = await new Chart(ctx, {
-        type: 'bubble',
-        data: {
-            xLabels: xAxisNeighborhood,
-            yLabels: yAxisCrimeType,
-            datasets: formattedData
-            },
-        options: {
-            title: {
-                display: true,
-                text: 'Denver Downtown Crime 2018'
-            }, scales: {
-                yAxes: [{ 
-                scaleLabel: {
-                    display: true,
-                    labelString: "Neighborhood"
-                }
-                }],
-                xAxes: [{ 
+    // const formattedData = await parsedData.map(dataObj => ({"label": "none", "data": [{...dataObj}], "backgroundColor": "#ff6384", 
+    // "hoverBackgroundColor": "#ff6384"}))
+    // TODO:Most likely change the tick labels to be categories, map to values for the actual data... let's see how this work
+    
+    const options = {
+        title: {
+            display: true,
+            text: 'Denver Downtown Crime 2018'
+        }, 
+        scales: {
+            yAxes: [{ 
                 scaleLabel: {
                     display: true,
                     labelString: "Crime Type"
-                }
-                }]
-            }
+                },
+                type: 'category',
+                labels: yAxisCrimeType
+            }],
+            xAxes: [{ 
+                scaleLabel: {
+                    display: true,
+                    labelString: "Neighborhood"
+                },
+                type: 'category',
+                labels: xAxisNeighborhood
+            }]
         }
-    })
+    }
 
-    // options: {
-    //     title: {
-    //       display: true,
-    //       text: 'Predicted world population (millions) in 2050'
-    //     }, scales: {
-    //       yAxes: [{ 
-    //         scaleLabel: {
-    //           display: true,
-    //           labelString: "Happiness"
-    //         }
-    //       }],
-    //       xAxes: [{ 
-    //         scaleLabel: {
-    //           display: true,
-    //           labelString: "GDP (PPP)"
-    //         }
-    //       }]
-    //     }
+    const myChart = await new Chart(ctx, {
+        type: 'bubble',
+        data: formattedData,
+        options: options
+    })
     
     console.log("I did it!", myChart)
-    console.table("parsedData", parsedData[0])
-    console.table("dataarray", formattedData)
+    console.table("parsedData", formattedData[0])
+    console.table("dataarray", parsedData)
 })()
 })
 
